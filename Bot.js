f54f98d1808a045173e131e3c64a7b6a00382c31
pedrodev777 <tobiasspedro594@gmail.com>
2021-07-09 20:47:07 -0300

    commit

--- Bot.js
+++ Bot.js
@@ -1,4 +1,7 @@
 const EventEmitter = require('@areojs/events');
+const { payload } = require('@areojs/constants');
+
+const WebSocket = require('ws');
 
 class Bot extends EventEmitter {
   constructor(token, options) {
@@ -10,6 +13,20 @@ class Bot extends EventEmitter {
       writable: true
     });
     
+  }
+  
+  async connect() {
+    this.ws = new WebSocket('wss://gateway.discord.gg/?v=9&encoding=json');
+    
+    this.ws.on('open', () => {
+      super.emit('open', this);
+      
+      payload.token = this.token;
+      
+      this.ws.send(JSON.stringify(payload))
+    });
+    
+    return this.token;
   }
 }
 
