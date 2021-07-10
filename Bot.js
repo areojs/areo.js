35cbe59ce6b6050d07f08a28df7e5db038d4781c
pedrodev777 <86243312+pedrodev777@users.noreply.github.com>
2021-07-10 14:36:29 -0300

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -4,6 +4,7 @@ const { payload } = require('@areojs/constants');
 const { Message } = require('@areojs/structures');
 
 const WebSocket = require('ws');
+const axios = require('axios');
 
 class Bot extends EventEmitter {
   constructor(token, options) {
@@ -16,11 +17,22 @@ class Bot extends EventEmitter {
       writable: true
     });
     
+    if(!this.token.startsWith('Bot') && options.tokenType == 'bot') {
+      this.token = 'Bot ' + this.token;
+    }
+    
     this._intervals = new Map();
     this._timeouts = new Map();
 
     this.readyAt = 0;
+    this.mobile = false;
     
+    if(options.mobile) {
+      
+      payload.properties.$browser = "Discord iOS";
+      this.mobile = true;
+      
+    }
   }
 
   get uptime() {
@@ -88,8 +100,8 @@ class Bot extends EventEmitter {
      let timeouts = Array.from(this._timeouts);
        
      for(let interval of intervals) {
-        clearInterval(interval[1])
-        this._intervals.delete(interval[0])
+        clearInterval(interval[1]);
+        this._intervals.delete(interval[0]);
      }
        
      for(let timeout of timeouts) {
@@ -97,8 +109,23 @@ class Bot extends EventEmitter {
         this._timeouts.delete(interval[0]);
      }
 
-     super.emit('debug', 'client destroyed.')
+     super.emit('debug', 'client destroyed.');
        
      return null;
+  }
+  
+  async createMessage(channelID, data) {
+    return this.DiscordRequest('POST', `channels/${channelID}/messages`, data);
+  }
+  
+  DiscordRequest(method, path, data, config = {}) {
+    let APIurl = 'https://discord.com/api/v9/';
+    
+    return axios[method.toLowerCase()](APIurl+path, data, {
+      headers: {
+        Authorization: this.token
+      }
+    });
+    
   }
 }
